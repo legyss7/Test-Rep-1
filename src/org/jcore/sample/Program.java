cb6556b7e8e9c670861e1ff24e7ff107520d17c8
legyss7 <legyss@mail.ru>
2023-11-29 13:32:50 +0600

    Update Program local git

--- src/org/jcore/sample/Program.java
+++ src/org/jcore/sample/Program.java
@@ -2,6 +2,7 @@ package testRep1.src.org.jcore.sample;
 
 public class Program {
     public static void main(String[] args) {
+        // конфликт local git
         System.out.println("Hello!");
     }
 }
