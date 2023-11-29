4aa98bc66e922fe4d4213dd85bb7c03c694f62ef
legyss7 <121185479+legyss7@users.noreply.github.com>
2023-11-29 13:35:37 +0600

    Update Program github2.java

--- src/org/jcore/sample/Program.java
+++ src/org/jcore/sample/Program.java
@@ -1,9 +1,9 @@
 package testRep1.src.org.jcore.sample;
 
 public class Program {
-    //конфликт github
+    //конфликт github.com
     public static void main(String[] args) {
-        // конфликт local git
+        // конфликт localgit
         System.out.println("Hello!");
     }
 }
